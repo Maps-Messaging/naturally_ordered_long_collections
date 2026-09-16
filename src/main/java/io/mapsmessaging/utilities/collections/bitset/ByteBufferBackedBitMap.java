f65c19c17ee43c282ca6e6ff1f048058cda7b239
Matthew Buckton <matthew@buckton.org>
2026-09-16 10:47:09 +0100

    fix(bitset): harden mapped bitmap operations
    Refs: MSG-307

--- src/main/java/io/mapsmessaging/utilities/collections/bitset/ByteBufferBackedBitMap.java
+++ src/main/java/io/mapsmessaging/utilities/collections/bitset/ByteBufferBackedBitMap.java
@@ -36,13 +36,11 @@ import java.util.ListIterator;
 @SuppressWarnings("squid:S7027")
 public class ByteBufferBackedBitMap implements BitSet {
 
-  private static final long LONG_MASK = 0xffffffffffffffffL;
+  private static final long LONG_MASK = -1L;
   private static final int LONG_BIT_SHIFT = 6;
-  private static final int BYTE_BIT_SHIFT = 3;
-  private static final int LONG_SIZE = 8;
-  private static final int LONG_BITS = 64;
+  private static final int LONG_SIZE = Long.BYTES;
+  private static final int LONG_BITS = Long.SIZE;
 
-  private final int longStartIdx;
   private final int longs;
   private final int capacity;
   private final int offset;
@@ -52,24 +50,25 @@ public class ByteBufferBackedBitMap implements BitSet {
   @Getter
   private long uniqueId;
 
-  // <editor-fold desc="BitSet constructors">
   public ByteBufferBackedBitMap(@NonNull @NotNull ByteBuffer buffer, int offset) {
     this(buffer, offset, 0);
   }
 
   public ByteBufferBackedBitMap(@NonNull @NotNull ByteBuffer buffer, int offset, long uniqueId) {
+    if (offset < 0 || offset > buffer.capacity()) {
+      throw new IndexOutOfBoundsException("Invalid byte buffer offset " + offset);
+    }
     this.uniqueId = uniqueId;
     backing = buffer;
-    int bits = backing.capacity() - offset;
-    bits = bits * LONG_SIZE;
-    longs = (bits >> LONG_BIT_SHIFT);
-    capacity = longs << 6L;
     this.offset = offset;
-    longStartIdx = getLongPosition(0);
+    int availableBytes = backing.capacity() - offset;
+    longs = availableBytes / LONG_SIZE;
+    if (longs <= 0) {
+      throw new IllegalArgumentException("ByteBuffer must contain at least one complete long");
+    }
+    capacity = longs * LONG_BITS;
   }
-  // </editor-fold>
 
-  // <editor-fold desc="Byte Buffer functions">
   public @NonNull @NotNull ByteBuffer getBacking() {
     return backing;
   }
@@ -84,338 +83,218 @@ public class ByteBufferBackedBitMap implements BitSet {
     return offset;
   }
 
-  // </editor-fold>
-
-  // <editor-fold desc="Single bit operations">
-
   @Override
   public boolean set(int bit) {
-    var internalBit = checkBoundary(bit);
-    var position = getLongPosition(internalBit);
-    var bitPos = internalBit % LONG_BITS;
-
-    long bitMask = 1L << (bitPos);
-
-    //
-    // Get the int and now set the bit within the long
-    //
-    var map = backing.getLong(position);
-    var original = map;
-    map |= bitMask;
-    backing.putLong(position, map);
-    return map != original;
+    int internalBit = checkBoundary(bit);
+    int wordIndex = internalBit >>> LONG_BIT_SHIFT;
+    long bitMask = 1L << internalBit;
+    long value = getWord(wordIndex);
+    long original = value;
+    value |= bitMask;
+    putWord(wordIndex, value);
+    return value != original;
   }
 
   @Override
   public boolean clear(int bit) {
-    var internalBit = checkBoundary(bit);
-    var position = getLongPosition(internalBit);
-    var bitPos = internalBit % LONG_BITS;
-
-    long bitMask = 1L << (bitPos);
-
-    //
-    // Get the long and now set the bit within the long
-    //
-    var map = backing.getLong(position);
-    var original = map;
-    map &= ~bitMask;
-    backing.putLong(position, map);
-    return map != original;
+    int internalBit = checkBoundary(bit);
+    int wordIndex = internalBit >>> LONG_BIT_SHIFT;
+    long bitMask = 1L << internalBit;
+    long value = getWord(wordIndex);
+    long original = value;
+    value &= ~bitMask;
+    putWord(wordIndex, value);
+    return value != original;
   }
 
   @Override
   public boolean isSet(int bit) {
-    var internalBit = checkBoundary(bit);
-    var position = getLongPosition(internalBit);
-    var bitPos = internalBit % LONG_BITS;
-
-    long bitMask = 1L << (bitPos);
-
-    //
-    // Get the long and now set the bit within the long
-    //
-    var map = backing.getLong(position);
-    return (map & bitMask) != 0;
+    int internalBit = checkBoundary(bit);
+    int wordIndex = internalBit >>> LONG_BIT_SHIFT;
+    long bitMask = 1L << internalBit;
+    return (getWord(wordIndex) & bitMask) != 0;
   }
 
   @Override
   public boolean isSetAndClear(int bit) {
-    var internalBit = checkBoundary(bit);
-    var position = getLongPosition(internalBit);
-    var bitPos = internalBit % LONG_BITS;
-
-    long bitMask = 1L << (bitPos);
-
-    //
-    // Get the long and now set the bit within the long
-    //
-    var map = backing.getLong(position);
-    boolean val = (map & bitMask) != 0;
-    if (val) {
-      map &= ~bitMask;
-      backing.putLong(position, map);
+    int internalBit = checkBoundary(bit);
+    int wordIndex = internalBit >>> LONG_BIT_SHIFT;
+    long bitMask = 1L << internalBit;
+    long value = getWord(wordIndex);
+    boolean set = (value & bitMask) != 0;
+    if (set) {
+      putWord(wordIndex, value & ~bitMask);
     }
-    return val;
+    return set;
   }
 
   @Override
   public void flip(int bit) {
-    var internalBit = checkBoundary(bit);
-    int position = getLongPosition(internalBit);
-    int bitPos = internalBit % LONG_BITS;
-
-    long bitMask = 1L << (bitPos);
-    var map = backing.getLong(position);
-    map ^= bitMask;
-    backing.putLong(position, map);
+    int internalBit = checkBoundary(bit);
+    int wordIndex = internalBit >>> LONG_BIT_SHIFT;
+    putWord(wordIndex, getWord(wordIndex) ^ (1L << internalBit));
   }
 
   @Override
   public void flip(int fromIndex, int toIndex) {
+    checkRange(fromIndex, toIndex);
     if (fromIndex == toIndex) {
       return;
     }
-    if (fromIndex > toIndex) {
-      throw new IndexOutOfBoundsException(
-          "fromIndex " + fromIndex + " is greater than toIndex " + toIndex);
-    }
 
-    int fromOffset = checkRangeBoundary(fromIndex);
-    int toOffset = checkRangeBoundary(toIndex);
-
-    int startWordIndex = getLongPosition(fromOffset);
-    int endWordIndex = getLongPosition(toOffset - 1);
-
-    int startBit = fromOffset % LONG_BITS;
-    int endBit = (toOffset - 1) % LONG_BITS;
-
-    long firstWordMask = LONG_MASK << startBit;
-    long lastWordMask = LONG_MASK >>> (LONG_BITS - 1 - endBit);
+    int startWordIndex = fromIndex >>> LONG_BIT_SHIFT;
+    int endWordIndex = (toIndex - 1) >>> LONG_BIT_SHIFT;
+    long firstWordMask = LONG_MASK << fromIndex;
+    long lastWordMask = LONG_MASK >>> -toIndex;
 
     if (startWordIndex == endWordIndex) {
-      long map = backing.getLong(startWordIndex);
-      map ^= firstWordMask & lastWordMask;
-      backing.putLong(startWordIndex, map);
+      putWord(startWordIndex, getWord(startWordIndex) ^ (firstWordMask & lastWordMask));
       return;
     }
 
-    long map = backing.getLong(startWordIndex);
-    map ^= firstWordMask;
-    backing.putLong(startWordIndex, map);
-
-    for (int position = startWordIndex + LONG_SIZE; position < endWordIndex; position += LONG_SIZE) {
-      map = backing.getLong(position);
-      map ^= LONG_MASK;
-      backing.putLong(position, map);
+    putWord(startWordIndex, getWord(startWordIndex) ^ firstWordMask);
+    for (int wordIndex = startWordIndex + 1; wordIndex < endWordIndex; wordIndex++) {
+      putWord(wordIndex, getWord(wordIndex) ^ LONG_MASK);
     }
-
-    map = backing.getLong(endWordIndex);
-    map ^= lastWordMask;
-    backing.putLong(endWordIndex, map);
+    putWord(endWordIndex, getWord(endWordIndex) ^ lastWordMask);
   }
 
-  private int checkRangeBoundary(int bit) {
-    if (bit < 0 || bit > capacity) {
-      throw new IndexOutOfBoundsException("Expecting range from 0 to " + capacity + " received " + bit);
-    }
-    return bit;
-  }
-
-  // </editor-fold>
-
-  // <editor-fold desc="Statistic functions">
-
-  /**
-   * Returns the total number of bits in the bit map that can be used
-   *
-   * @return the number of bits in the map
-   */
   @Override
   public int length() {
     return capacity;
   }
 
-  /**
-   * Returns the number of set bits in the bit map
-   *
-   * @return the number of currently set bits
-   */
   @Override
   public int cardinality() {
-    var sum = 0;
-    for (var x = 0; x < longs; x++) {
-      int pos = (x << BYTE_BIT_SHIFT) + longStartIdx;
-      sum += Long.bitCount(backing.getLong(pos));
+    int sum = 0;
+    for (int wordIndex = 0; wordIndex < longs; wordIndex++) {
+      sum += Long.bitCount(getWord(wordIndex));
     }
     return sum;
   }
 
   @Override
   public boolean isEmpty() {
-    for (var x = 0; x < longs; x++) {
-      int pos = (x << BYTE_BIT_SHIFT) + longStartIdx;
-      if (backing.getLong(pos) != 0) {
+    for (int wordIndex = 0; wordIndex < longs; wordIndex++) {
+      if (getWord(wordIndex) != 0) {
         return false;
       }
     }
     return true;
   }
 
-  // </editor-fold>
-
-  // <editor-fold desc="Search Functions">
   @Override
   public int nextSetBit(int fromIndex) {
     if (fromIndex < 0 || fromIndex >= capacity) {
       return -1;
     }
 
-    var internalBit = checkBoundary(fromIndex);
-    int position = getLongPosition(internalBit);
-    long word = backing.getLong(position) & (LONG_MASK << internalBit);
-
+    int wordIndex = fromIndex >>> LONG_BIT_SHIFT;
+    long word = getWord(wordIndex) & (LONG_MASK << fromIndex);
     while (true) {
       if (word != 0) {
-        int base = (position - longStartIdx) << 3;
-        base += Long.numberOfTrailingZeros(word);
-        return base;
+        return (wordIndex * LONG_BITS) + Long.numberOfTrailingZeros(word);
       }
-      position += LONG_SIZE;
-      if (position == longs << 3) {
+      if (++wordIndex >= longs) {
         return -1;
       }
-      word = backing.getLong(position);
+      word = getWord(wordIndex);
     }
   }
 
   @Override
   public int nextSetBitAndClear(int fromIndex) {
-    var internalBit = checkBoundary(fromIndex);
-    int position = getLongPosition(internalBit);
-    long word = backing.getLong(position) & (LONG_MASK << internalBit);
-
-    while (true) {
-      if (word != 0) {
-        int base = (position - longStartIdx) << 3;
-        base += Long.numberOfTrailingZeros(word);
-
-        //
-        // Get the long and now set the bit within the long
-        //
-        long bitMask = 1L << (base);
-        word &= ~bitMask;
-        backing.putLong(position, word);
-        return base;
-      }
-      position += LONG_SIZE;
-      if (position == longs << 3) {
-        return -1;
-      }
-      word = backing.getLong(position);
+    int bit = nextSetBit(fromIndex);
+    if (bit >= 0) {
+      clear(bit);
     }
+    return bit;
   }
 
   @Override
   public int nextClearBit(int fromIndex) {
-    var internalBit = checkBoundary(fromIndex);
-    int position = getLongPosition(internalBit);
+    if (fromIndex < 0 || fromIndex >= capacity) {
+      return -1;
+    }
 
-    long word = ~backing.getLong(position);
+    int wordIndex = fromIndex >>> LONG_BIT_SHIFT;
+    long word = ~getWord(wordIndex) & (LONG_MASK << fromIndex);
     while (true) {
       if (word != 0) {
-        int base = (position - longStartIdx) << 3;
-        base += Long.numberOfTrailingZeros(word);
-        return base;
+        return (wordIndex * LONG_BITS) + Long.numberOfTrailingZeros(word);
       }
-      position += LONG_SIZE;
-      if (position == longs << 3) {
+      if (++wordIndex >= longs) {
         return -1;
       }
-      word = ~backing.getLong(position);
+      word = ~getWord(wordIndex);
     }
   }
 
   @Override
   public int previousSetBit(int fromIndex) {
-    if (fromIndex < 0 || fromIndex >= capacity) {
+    if (fromIndex < 0) {
       return -1;
     }
-    var internalBit = checkBoundary(fromIndex);
-    int position = getLongPosition(internalBit);
-    int bitPos = (fromIndex + 1) % LONG_BITS;
-    long word = backing.getLong(position) & (LONG_MASK >>> (LONG_BITS - bitPos));
-
+    int index = Math.min(fromIndex, capacity - 1);
+    int wordIndex = index >>> LONG_BIT_SHIFT;
+    long word = getWord(wordIndex) & (LONG_MASK >>> -(index + 1));
     while (true) {
       if (word != 0) {
-        int base = ((position - longStartIdx) << 3) + 64;
-        base = base - 1 - Long.numberOfLeadingZeros(word);
-        return base;
+        return ((wordIndex + 1) * LONG_BITS) - 1 - Long.numberOfLeadingZeros(word);
       }
-      position -= LONG_SIZE;
-      if (position < 0) {
+      if (--wordIndex < 0) {
         return -1;
       }
-      word = backing.getLong(position);
+      word = getWord(wordIndex);
     }
   }
 
+  @Override
   public int previousClearBit(int fromIndex) {
-    if (fromIndex < 0 || fromIndex >= capacity) {
+    if (fromIndex < 0) {
       return -1;
     }
-
-    var internalBit = checkBoundary(fromIndex);
-    int position = getLongPosition(internalBit);
-    int bitPos = (fromIndex + 1) % LONG_BITS;
-    long word = ~backing.getLong(position) & (LONG_MASK >>> (LONG_BITS - bitPos));
-
+    int index = Math.min(fromIndex, capacity - 1);
+    int wordIndex = index >>> LONG_BIT_SHIFT;
+    long word = ~getWord(wordIndex) & (LONG_MASK >>> -(index + 1));
     while (true) {
       if (word != 0) {
-        int base = ((position - longStartIdx) << 3) + LONG_BITS;
-        base = base - 1 - Long.numberOfLeadingZeros(word);
-        return base;
+        return ((wordIndex + 1) * LONG_BITS) - 1 - Long.numberOfLeadingZeros(word);
       }
-      position -= LONG_SIZE;
-      if (position < 0) {
+      if (--wordIndex < 0) {
         return -1;
       }
-      word = ~backing.getLong(position);
+      word = ~getWord(wordIndex);
     }
   }
-  // </editor-fold>
 
-  // <editor-fold desc="Complete bit map operations">
   @Override
   public void clear() {
-    for (var x = 0; x < longs; x++) {
-      int pos = (x << BYTE_BIT_SHIFT) + longStartIdx;
-      backing.putLong(pos, 0);
+    for (int wordIndex = 0; wordIndex < longs; wordIndex++) {
+      putWord(wordIndex, 0L);
     }
   }
 
   @Override
   public String toString() {
-    var sb = new StringBuilder();
+    StringBuilder sb = new StringBuilder();
     sb.append("ID:").append(uniqueId);
     sb.append(" Size:").append(cardinality()).append(" Offset:").append(offset).append(" {");
-    var counter = 0;
-    var idx = 0;
-    while (counter < 1024 && idx < capacity) {
-      idx = nextSetBit(idx);
-      if (idx < 0) {
+    int counter = 0;
+    int index = 0;
+    while (counter < 1024 && index < capacity) {
+      index = nextSetBit(index);
+      if (index < 0) {
         break;
       }
-      sb.append(idx).append(", ");
-      idx++;
+      sb.append(index).append(", ");
+      index++;
       counter++;
     }
     sb.append("}");
     return sb.toString();
   }
-  // </editor-fold>
 
-  // <editor-fold desc="Bitwise operations between 2 BitMaps">
   @Override
   public void and(BitSet map) {
     bitwiseCompute(map, new And());
@@ -438,62 +317,55 @@ public class ByteBufferBackedBitMap implements BitSet {
 
   private void bitwiseCompute(BitSet test, BitWiseOperator operator) {
     if (test instanceof ByteBufferBackedBitMap map) {
-      int len = Math.min(map.longs, longs);
-      for (var x = 0; x < len; x++) {
-        int position = (x << BYTE_BIT_SHIFT) + longStartIdx;
-        var lhs = backing.getLong(position);
-        var rhs = map.backing.getLong(position);
-        long result = operator.operation(lhs, rhs);
-        backing.putLong(position, result);
+      for (int wordIndex = 0; wordIndex < longs; wordIndex++) {
+        long rhs = wordIndex < map.longs ? map.getWord(wordIndex) : 0L;
+        putWord(wordIndex, operator.operation(getWord(wordIndex), rhs));
       }
     } else if (test instanceof BitSetImpl map) {
       long[] values = map.getWords();
-      var rhsIndex = 0;
-      int len = Math.min(values.length, longs);
-      for (var x = 0; x < len; x++) {
-        int position = (x << BYTE_BIT_SHIFT) + longStartIdx;
-        var lhs = backing.getLong(position);
-        var rhs = values[rhsIndex];
-        rhsIndex++;
-        long result = operator.operation(lhs, rhs);
-        backing.putLong(position, result);
+      for (int wordIndex = 0; wordIndex < longs; wordIndex++) {
+        long rhs = wordIndex < values.length ? values[wordIndex] : 0L;
+        putWord(wordIndex, operator.operation(getWord(wordIndex), rhs));
       }
     } else {
-      throw new UnsupportedOperationException(
-          "Unable to perform bitwise operation from " + test.getClass());
+      throw new UnsupportedOperationException("Unable to perform bitwise operation from " + test.getClass());
     }
   }
 
   public int getLongCount() {
     return longs;
   }
-  // </editor-fold>
 
-  // <editor-fold desc="positional functions">
   private int checkBoundary(int bit) {
-    //
-    // Must be within range
-    //
     if (bit < 0 || bit >= capacity) {
-      throw new IndexOutOfBoundsException("Expecting range from 0 to " + (capacity) + " received " + bit);
+      throw new IndexOutOfBoundsException(
+          "Expecting range from 0 to " + (capacity - 1) + " received " + bit);
     }
     return bit;
   }
 
-  private int getLongPosition(int bit) {
-    //
-    // Must be within bit set
-    //
-    int position = (bit >> LONG_BIT_SHIFT);
-
-    if (position > longs) {
-      throw new IndexOutOfBoundsException();
+  private void checkRange(int fromIndex, int toIndex) {
+    if (fromIndex < 0 || fromIndex > capacity || toIndex < 0 || toIndex > capacity || fromIndex > toIndex) {
+      throw new IndexOutOfBoundsException(
+          "Expecting range within 0 to " + capacity + " received " + fromIndex + " to " + toIndex);
     }
-    return (position << BYTE_BIT_SHIFT) + offset; // Return position in bytes
   }
-  // </editor-fold>
 
-  // <editor-fold desc="Iterator functions">
+  private int wordPosition(int wordIndex) {
+    if (wordIndex < 0 || wordIndex >= longs) {
+      throw new IndexOutOfBoundsException("Invalid long index " + wordIndex);
+    }
+    return offset + (wordIndex * LONG_SIZE);
+  }
+
+  private long getWord(int wordIndex) {
+    return backing.getLong(wordPosition(wordIndex));
+  }
+
+  private void putWord(int wordIndex, long value) {
+    backing.putLong(wordPosition(wordIndex), value);
+  }
+
   @Override
   public Iterator<Integer> iterator() {
     return new BitSetIterator(this);
@@ -505,8 +377,6 @@ public class ByteBufferBackedBitMap implements BitSet {
   }
 
   long getLong(int index) {
-    int position = (index << BYTE_BIT_SHIFT) + offset;
-    return backing.getLong(position);
+    return getWord(index);
   }
-  // </editor-fold>
 }
